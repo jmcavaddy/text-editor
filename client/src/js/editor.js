fbe201ffb2242c0f6691f3a0b4fd5c0d2e451c53
jmcavaddy <jakemcavaddy@gmail.com>
2023-06-07 05:18:27 -0400

    Addressed TODOs

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -25,15 +25,15 @@ export default class {
     // When the editor is ready, set the value to whatever is stored in indexeddb.
     // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
     getDb().then((data) => {
-      console.info('Loaded data from IndexedDB, injecting into editor');
-      this.editor.setValue(data || localData || header);
+      console.info('Loaded data', data, ' from IndexedDB, injecting into editor');
+      this.editor.setValue( data || localData || header );
     });
-
+    
     this.editor.on('change', () => {
       localStorage.setItem('content', this.editor.getValue());
     });
 
-    // Save the content of the editor when the editor itself is loses focus
+    // Save the content of the editor when the editor itself loses focus
     this.editor.on('blur', () => {
       console.log('The editor has lost focus');
       putDb(localStorage.getItem('content'));
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,8 +3,6 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
-// TODO: Add CSS loaders and babel to webpack.
 
 module.exports = () => {
   return {
@@ -13,6 +11,8 @@ module.exports = () => {
       main: './src/js/index.js',
       install: './src/js/install.js',
       editor: './src/js/editor.js',
+      database: './src/js/database.js',
+      header: './src/js/header.js',
     },
     output: {
       filename: '[name].bundle.js',
@@ -66,7 +66,7 @@ module.exports = () => {
             loader: 'babel-loader',
             options: {
               presets: ['@babel/preset-env'],
-              // plugins: ['@babel/plugin-proposal-object-rest-spread', '@babel/transform-runtime'],
+              plugins: ['@babel/transform-runtime'],
             },
           },
         },
